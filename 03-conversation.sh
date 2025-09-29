cc17235dcd86df4d9e4f1b33a435306252aebf08
akshay reddy <ardaws25@gmail.com>
2025-09-29 06:31:35 +0530

    conversation

---
+++ 03-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1:akshay
+person2:agastya
+
+echo "$person1 :: Hi $person2, How r u?"
+echo "$person2 :: Hello $person1 ,I'm fine. How r u ?"
+echo "$person1 :: what are u learning currrently"
+echo "$person2 :: Learing Shell Script"
